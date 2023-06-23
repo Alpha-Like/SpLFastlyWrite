6337ad13193e71724c7d89b773ad6c2805e7af68
Keshava Tripathi <124826956+ShutupKeshav@users.noreply.github.com>
2023-06-23 14:03:29 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+# DO NOT FILL ANYTHING HERE, FILL EVERYTHING IN ENVIRONMENT ITSELF !
+
+# mandatory vars
+BOT_TOKEN = os.getenv("BOT_TOKEN", None) # Get it from @BotFather [Telegram]
+MONGO_DB_URI = os.getenv("MONGO_DB_URI", None) # Get it from mongodb.com
+OWNER_ID = int(os.getenv("OWNER_ID", '')) # Get it from @SpL_Levi_Ackerman_Bot [Telegram]
+
+# non-mandatory vars
+API_ID = os.getenv("API_ID", None)
+API_HASH = os.getenv("API_HASH", None)
+BACKGROUND_IMAGE_URL = os.getenv("BACKGROUND_IMAGE_URL", None) # use Telegraph 
+WORD_SPAWN_TIME = os.getenv("WORD_SPAWN_TIME", None) # must be in seconds, Ex :- 900, Minimum :- 900
+SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", None) # Ex :- 'Spoiled_Community' 
+SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", None) # Ex :- 'SpLBots'
