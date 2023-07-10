073c5b3ada109bd56ea950c685f7285f3a87287a
Keshava Tripathi <124826956+ShutupKeshav@users.noreply.github.com>
2023-07-10 13:29:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,33 +14,3 @@ BACKGROUND_IMAGE_URL = os.getenv("BACKGROUND_IMAGE_URL", None) # use Telegraph
 WORD_SPAWN_TIME = os.getenv("WORD_SPAWN_TIME", None) # must be in seconds, Ex :- 900, Minimum :- 900
 SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", None) # Ex :- 'Spoiled_Community' 
 SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", None) # Ex :- 'SpLBots'
-
-# do not change code below
-if API_ID:
-  API_ID = int(API_ID)
-  
-if WORD_SPAWN_TIME:
-  try:
-    WORD_SPAWN_TIME = int(WORD_SPAWN_TIME)
-    if WORD_SPAWN_TIME < 900:
-      WORD_SPAWN_TIME = 900
-  except:
-    WORD_SPAWN_TIME = 900
-else:
-  WORD_SPAWN_TIME = 900
-
-if SUPPORT_GROUP:
-  if SUPPORT_GROUP[0] == '@':
-    SUPPORT_GROUP = SUPPORT_GROUP[1:]
-else:
-  SUPPORT_GROUP = 'Spoiled_Community'
-  
-if SUPPORT_CHANNEL:
-  if SUPPORT_CHANNEL[0] == '@':
-    SUPPORT_CHANNEL = SUPPORT_GROUP[1:]
-else:
-  SUPPORT_CHANNEL = 'SpLBots'
-
-if not BACKGROUND_IMAGE_URL:
-  print('Using Default Background Image...')
-  BACKGROUND_IMAGE_URL = 'https://telegra.ph/file/fe73eca247a491d4a6927.jpg'
